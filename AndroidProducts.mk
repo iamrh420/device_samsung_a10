3ffa2535ca933019a8d6401a13f1054fb4097ec9
iamrh420 <92685435+iamrh420@users.noreply.github.com>
2023-10-26 01:23:52 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,7 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_a10.mk
+    $(LOCAL_DIR)/lineage_a10.mk
 
 COMMON_LUNCH_CHOICES := \
-    $(foreach variant, user userdebug eng, lineage_a10-$(variant))
+    lineage_a10-user \
+    lineage_a10-userdebug \
+    lineage_a10-eng
