4ac643dd5b037ae92d6d4ec0db5b313860dc5c6e
iamrh420 <92685435+iamrh420@users.noreply.github.com>
2023-10-26 01:31:42 +0600

    Update and rename cherish_a10.mk to lineage_a10.mk

--- lineage_a10.mk
+++ lineage_a10.mk
@@ -11,26 +11,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/a10/device.mk)
 
 # Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
-PRODUCT_NAME := cherish_a10
+PRODUCT_NAME := lineage_a10
 PRODUCT_MODEL := SM-A105F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-google
 
-BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ2A.230505.002/9891397:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ3A.230705.001/10216780:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230505.002 9891397 release-keys"
+	PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230705.001 10216780 release-keys"
 
 # Rom Specific Flags
+TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=Sergeant Kakashi
-CHERISH_BUILD_TYPE := OFFICIAL
+# Maintainer Flags
+ALPHA_BUILD_TYPE := UNOFFICIAL
+ALPHA_MAINTAINER := Rakib/tg:@iamr1819
+WITH_GAPPS := false
